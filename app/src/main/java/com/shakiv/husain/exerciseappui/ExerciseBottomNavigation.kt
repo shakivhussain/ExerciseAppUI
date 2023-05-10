d41f4d02991af73d09820b90b6140e1ffd84f8eb
Shakiv Hussain <51313301+shakivhussain@users.noreply.github.com>
2023-05-10 15:51:36 +0530

    ✅ Bottom navigation - Material

---
+++ app/src/main/java/com/shakiv/husain/exerciseappui/ExerciseBottomNavigation.kt
@@ -0,0 +1,40 @@
+package com.shakiv.husain.exerciseappui
+
+import androidx.compose.material.BottomNavigation
+import androidx.compose.material.BottomNavigationItem
+import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.Spa
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+
+@Preview
+@Composable
+private fun ExerciseBottomNavigation(modifier: Modifier = Modifier) {
+    BottomNavigation(
+        modifier,
+        backgroundColor = MaterialTheme.colors.background
+    ) {
+
+        BottomNavigationItem(
+            icon = { Icon(imageVector = Icons.Default.Spa, contentDescription = null) },
+            label = { Text(stringResource(R.string.bottom_navigation_home)) },
+            selected = true,
+            onClick = {}
+        )
+        BottomNavigationItem(
+            icon = { Icon(imageVector = Icons.Default.AccountCircle, contentDescription = null) },
+            label = { Text(stringResource(id = R.string.bottom_navigation_profile)) },
+            selected = false,
+            onClick = {}
+        )
+
+    }
+
+
+}
