105f49de88927a0ed3760c452e651652c14f46bf
Shakiv Hussain <51313301+shakivhussain@users.noreply.github.com>
2023-05-10 13:08:00 +0530

    ✅ Searchbar Base Create

--- app/src/main/java/com/shakiv/husain/exerciseappui/MainActivity.kt
+++ app/src/main/java/com/shakiv/husain/exerciseappui/MainActivity.kt
@@ -5,10 +5,21 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.heightIn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.shakiv.husain.exerciseappui.ui.theme.ExerciseAppUITheme
@@ -26,7 +37,7 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MyApp() {
-
+    SearchBar()
 }
 
 @Composable
@@ -41,13 +52,26 @@ fun MyAppReview() {
 fun SearchBar(
     modifier: Modifier = Modifier
 ) {
-
+    var text by remember {
+        mutableStateOf("")
+    }
     TextField(
-        value = "",
-        onValueChange = {},
+        value = text,
+        onValueChange = {
+            text = it
+        },
+
+        leadingIcon = {
+            Icon(imageVector = Icons.Default.Search, contentDescription = null)
+        },
+        colors = TextFieldDefaults.textFieldColors(
+            containerColor = MaterialTheme.colorScheme.surface
+        ),
+        placeholder = {
+            Text(text = stringResource(id = R.string.placeholder_search))
+        },
         modifier = modifier
             .fillMaxWidth()
             .heightIn(min = 56.dp)
     )
-
 }
--- app/src/main/java/com/shakiv/husain/exerciseappui/ui/theme/Theme.kt
+++ app/src/main/java/com/shakiv/husain/exerciseappui/ui/theme/Theme.kt
@@ -18,13 +18,16 @@ import androidx.core.view.WindowCompat
 private val DarkColorScheme = darkColorScheme(
     primary = Purple80,
     secondary = PurpleGrey80,
-    tertiary = Pink80
+    tertiary = Pink80,
+    surface = PurpleGrey80
 )
 
 private val LightColorScheme = lightColorScheme(
     primary = Purple40,
     secondary = PurpleGrey40,
-    tertiary = Pink40
+    tertiary = Pink40,
+    surface = PurpleGrey80
+
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
