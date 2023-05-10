32b7c4761b87fb6a6a8f2e73db602e613bf38532
Shakiv Hussain <51313301+shakivhussain@users.noreply.github.com>
2023-05-10 14:51:19 +0530

    ✅ Home section - Slot APIs

--- app/src/main/java/com/shakiv/husain/exerciseappui/MainActivity.kt
+++ app/src/main/java/com/shakiv/husain/exerciseappui/MainActivity.kt
@@ -46,6 +46,7 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.shakiv.husain.exerciseappui.ui.theme.ExerciseAppUITheme
+import java.util.Locale
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -68,6 +69,35 @@ fun MyApp() {
 @Composable
 fun MyAppReview() {
     SearchBar()
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun HomeSectionPreview() {
+
+    HomeSection(R.string.ab1_inversions, content = {
+        AlignYourBodyRow(modifier = Modifier, alignYourBodyData)
+    })
+}
+
+@Composable
+fun HomeSection(
+    @StringRes title: Int,
+    modifier: Modifier = Modifier,
+    content: @Composable () -> Unit
+) {
+
+    Column(modifier) {
+        Text(
+            stringResource(title).uppercase(Locale.getDefault()),
+            style = MaterialTheme.typography.titleMedium,
+            modifier = Modifier.paddingFromBaseline(top = 40.dp, bottom = 8.dp)
+                .padding(horizontal = 16.dp)
+        )
+        content()
+    }
+
 }
 
 
@@ -210,7 +240,7 @@ private fun AlignYourBodyRow(
 
 @Preview
 @Composable
-fun FavoriteCollectionGridPreview(){
+fun FavoriteCollectionGridPreview() {
     FavoriteCollectionGrid()
 }
 
