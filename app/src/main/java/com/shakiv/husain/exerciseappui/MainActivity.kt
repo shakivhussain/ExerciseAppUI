f4974bd4386d9d60e346fd3b96322416d18a38a3
Shakiv Hussain <51313301+shakivhussain@users.noreply.github.com>
2023-05-10 13:26:30 +0530

    ✅ Align your body - Alignment

--- app/src/main/java/com/shakiv/husain/exerciseappui/MainActivity.kt
+++ app/src/main/java/com/shakiv/husain/exerciseappui/MainActivity.kt
@@ -3,8 +3,16 @@ package com.shakiv.husain.exerciseappui
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.paddingFromBaseline
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -18,7 +26,11 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -38,8 +50,14 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun MyApp() {
     SearchBar()
+//    AlignYourBodyElement()
+    AlignYourBodyElement(
+        drawableRes = R.drawable.ab1_inversions,
+        text = R.string.ab1_inversions
+    )
 }
 
+@Preview(showBackground = true)
 @Composable
 fun MyAppReview() {
     SearchBar()
@@ -47,7 +65,7 @@ fun MyAppReview() {
 
 
 @OptIn(ExperimentalMaterial3Api::class)
-@Preview(showBackground = true)
+
 @Composable
 fun SearchBar(
     modifier: Modifier = Modifier
@@ -74,4 +92,43 @@ fun SearchBar(
             .fillMaxWidth()
             .heightIn(min = 56.dp)
     )
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun AlignYourBodyElementPreview() {
+    AlignYourBodyElement(
+        drawableRes = R.drawable.ab1_inversions,
+        text = R.string.ab1_inversions,
+        modifier = Modifier.padding(8.dp)
+    )
+}
+
+@Composable
+fun AlignYourBodyElement(
+    @DrawableRes drawableRes: Int,
+    @StringRes text: Int,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally,
+
+        ) {
+        Image(
+            painter = painterResource(drawableRes),
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .size(88.dp)
+                .clip(CircleShape)
+        )
+        Text(
+            text = stringResource(text),
+            style = MaterialTheme.typography.titleMedium,
+            modifier = Modifier.paddingFromBaseline(top = 24.dp, bottom = 8.dp)
+        )
+    }
+
 }
